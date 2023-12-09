5846cc7f6b19f5b60c5c43b9a62b530a141d6334
mushrune <112779587+mushrune@users.noreply.github.com>
2023-12-08 19:14:30 -0800

    Additional navigation menu updates. Backdrop blur functionality.

--- tg-react-app/src/Components/Navigator.tsx
+++ tg-react-app/src/Components/Navigator.tsx
@@ -18,46 +18,51 @@ const navButtonStyle: string = "h-full w-fit lowercase italic font-normal text-w
 
 const Navigator: React.FC = () => {
     return(
-        <div className="
-            max-w-4xl w-full sm:w-[95%] h-20 sm:h-32
-            mb-1 sm:mb-5 mt-2 mx-auto flex
-        ">
-            <div className="mx-2 flex-1 flex flex-col">
-                {title}
-                <div className="
-                    w-[98%] h-8 sm:h-14 mt-auto mb-2 mx-auto rounded-md
-                    border border-solid border-white border-[1px]
-                    flex justify-around
-                ">
-                    <Button
-                        size="small"
-                        variant="text"
-                        className={navButtonStyle}
-                    >
-                        about
-                    </Button>
-                    <Button
-                        size="small"
-                        variant="text"
-                        className={navButtonStyle}
-                    >
-                        projects
-                    </Button>
-                    <img src="/logo.svg" alt="puck logo" className="h-full py-1 sm:py-2 ml-2 invert"/>
-                    <Button
-                        size="small"
-                        variant="text"
-                        className={navButtonStyle}
-                    >
-                        contact
-                    </Button>
-                    <Button
-                        size="small"
-                        variant="text"
-                        className={navButtonStyle}
-                    >
-                        blog
-                    </Button>
+        <div>
+            <div className="w-full h-20 sm:h-32 mb-1 sm:mb-5 mt-2" />
+            <div className="
+                fixed top-0
+                w-full h-20 sm:h-32
+                mb-1 sm:mb-5 pt-1 px-0 sm:px-4 flex
+                z-10 backdrop-blur-3xl backdrop-brightness-75
+            ">
+                <div className="px-2 mx-auto max-w-4xl flex-1 flex flex-col">
+                    {title}
+                    <div className="
+                        w-[98%] h-8 sm:h-14 mt-auto mb-2 mx-auto rounded-md
+                        border border-solid border-white border-[1px]
+                        flex justify-around
+                    ">
+                        <Button
+                            size="small"
+                            variant="text"
+                            className={navButtonStyle}
+                        >
+                            about
+                        </Button>
+                        <Button
+                            size="small"
+                            variant="text"
+                            className={navButtonStyle}
+                        >
+                            projects
+                        </Button>
+                        <img src="/logo.svg" alt="puck logo" className="h-full py-1 sm:py-2 ml-2 invert"/>
+                        <Button
+                            size="small"
+                            variant="text"
+                            className={navButtonStyle}
+                        >
+                            contact
+                        </Button>
+                        <Button
+                            size="small"
+                            variant="text"
+                            className={navButtonStyle}
+                        >
+                            blog
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
--- tg-react-app/src/Pages/Home.tsx
+++ tg-react-app/src/Pages/Home.tsx
@@ -4,7 +4,7 @@ import {Paper, Typography} from "@mui/material";
 const Home: React.FC = () => {
 
     return(
-        <Paper className="w-full h-80 pt-4 px-8" elevation={0}>
+        <Paper className="w-full h-[1000px] pt-4 px-8" elevation={0}>
             <Typography variant="h1">Blog</Typography>
         </Paper>
     )
