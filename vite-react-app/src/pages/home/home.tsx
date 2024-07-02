9889f22e69bd7f98ef5f9c0432b699187ac09a71
mushrune <112779587+mushrune@users.noreply.github.com>
2024-07-01 17:49:21 -0700

    Removed create-react-app, added Vite.

--- vite-react-app/src/pages/home/home.tsx
+++ vite-react-app/src/pages/home/home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import LandingGraphic from "./components/landing_graphic/landing_graphic";
-import {Typography} from "@mui/material";
+import Caption from "./components/caption";
 
 const Home: React.FC = () => {
     return(
         <>
             <LandingGraphic />
-            <Typography variant="subtitle1" className="w-full text-center">Transistor Garden is a software and technology business owned and operated by Phoebe Fox</Typography>
+            <Caption />
         </>
     )
 }
