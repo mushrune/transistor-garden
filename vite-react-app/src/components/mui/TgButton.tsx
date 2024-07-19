bcd51034fd2a561006b2fdf2db0c8cda0110ef9f
mushrune <112779587+mushrune@users.noreply.github.com>
2024-07-18 18:39:40 -0700

    tweaks to the button to change the behavior of the contained hover state.

--- vite-react-app/src/components/mui/TgButton.tsx
+++ vite-react-app/src/components/mui/TgButton.tsx
@@ -2,27 +2,28 @@ import * as React from 'react';
 import Button, { ButtonProps } from "@mui/material/Button";
 import { styled } from '@mui/material/styles';
 
-const StyledButton = styled(Button)<ButtonProps>(({ theme }) => ({
-    color: theme.palette?.primary,
-    borderColor: theme.palette?.primary,
-    fontWeight: 'normal',
-    fontStyle: 'italic',
-    textTransform: 'lowercase',
-    padding: '5px 30px',
-    '&:hover': {
-        color: theme.palette.primary.main,
-        backgroundColor: theme.palette.background.paper
-
-    },
-    '&.MuiButton-contained': {
-        color: theme.palette?.secondary,
-        backgroundColor: theme.palette?.primary
-    },
-    '&.Mui-disabled': {
-        color: 'grey',
-        backgroundColor: theme.palette?.background?.paper
-    }
-}));
+const StyledButton = styled(Button)<ButtonProps>(({ theme }) => {
+    return ({
+        color: theme.palette?.primary.main,
+        borderColor: theme.palette?.primary.main,
+        fontWeight: 'normal',
+        fontStyle: 'italic',
+        textTransform: 'lowercase',
+        padding: '5px 30px',
+        '&.MuiButton-contained': {
+            color: 'black',
+            backgroundColor: theme.palette?.primary.main,
+            '&:hover': {
+                color: '#ffffff',
+                backgroundColor: theme.palette?.primary.dark
+            }
+        },
+        '&.Mui-disabled': {
+            color: theme.palette?.text.disabled,
+            backgroundColor: theme.palette?.background?.paper
+        }
+    });
+});
 
 const TgButton: React.FC<ButtonProps> = ( props ) => (
     <StyledButton {...props}>
